b40641025a305534deb3dbe9bab9b188d6e09a60
dandyZicky <dandyzicky1@gmail.com>
2023-10-15 02:55:17 +0700

    sql table quick start

---
+++ sql/roast_table.sql
@@ -0,0 +1,21 @@
+DROP TABLE roasters CASCADE;
+DROP TABLE roast_sessions CASCADE;
+DROP TABLE session_measurements CASCADE;
+
+CREATE TABLE roasters (
+  id BIGSERIAL PRIMARY KEY,
+  serial_number VARCHAR(36) UNIQUE NOT NULL
+);
+
+CREATE TABLE roast_sessions (
+  id BIGSERIAL PRIMARY KEY,
+  roaster_id INTEGER REFERENCES roasters(id),
+  roast_date timestamp NOT NULL
+);
+
+CREATE TABLE session_measurements (
+  session_id INTEGER PRIMARY KEY REFERENCES roast_sessions(id),
+  suhu DOUBLE PRECISION
+);
+
+INSERT INTO roasters (serial_number) VALUES ('f25a87c9-4613-49da-a61d-b16f352441ad')
